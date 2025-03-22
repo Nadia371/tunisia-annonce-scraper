ef5f0edda7b3cbaa033cd3d8eff7556769fa819d
Nadia371 <trabelsinadia371@gmail.com>
2025-03-22 21:58:44 +0100

    Updated scraper to handle edge cases

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -85,7 +85,7 @@ def save_to_postgres(data):
 
 # Base URL (same as your friend's)
 base_url = "http://www.tunisie-annonce.com/AnnoncesImmobilier.asp?rech_cod_cat=1&rech_cod_rub=&rech_cod_typ=&rech_cod_sou_typ=&rech_cod_pay=TN&rech_cod_reg=&rech_cod_vil=&rech_cod_loc=&rech_prix_min=&rech_prix_max=&rech_surf_min=&rech_surf_max=&rech_age=&rech_photo=&rech_typ_cli=&rech_order_by=31"
-num_pages = 10  # Number of pages to scrape
+num_pages = 100  # Number of pages to scrape
 
 # Scrape all pages
 listings_data = scrape_all_pages(base_url, num_pages)
